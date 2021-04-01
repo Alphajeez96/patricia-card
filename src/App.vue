ff90b578704c97cc4b154ee1dc1e6f85d7cd5e69
Prince <chimeremezechukwudire@gmail.com>
2021-04-02 00:19:33 +0100

    folder structure added

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="center">
+      <card />
+    </div>
+  </div>
+</template>
+
+<script>
+import card from "./components/card";
+
+export default {
+  name: "App",
+  components: {
+    card,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Graphik";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.center {
+  width: 499px;
+  height: 305px;
+  position: absolute; /*Can also be `fixed`*/
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  /*Solves a problem in which the content is being cut when the div is smaller than its' wrapper:*/
+  max-width: 100%;
+  max-height: 100%;
+  overflow: auto;
+}
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="card-holder">
+    <div class="flex">
+    <!-- Logo here-->
+    <div></div>
+
+   <!-- Visibility Toggler here-->
+    <div class="ml-auto"></div>
+    </div>
+    
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+};
+</script>
+
+<style scoped>
+.card-holder {
+  background: var(--primary-black);
+
+}
+</style>
