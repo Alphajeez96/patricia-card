a9ee0156697ad3e5a2ffea1f41924765b06b521b
Prince <chimeremezechukwudire@gmail.com>
2021-04-02 00:28:37 +0100

    feature: added card layout

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <div class="card-holder">
-    <div class="flex">
-    <!-- Logo here-->
-    <div></div>
+      <div class="flex">
+        <!-- Logo here-->
+        <div>
+        <img src="@/assets/img/logo.svg" height="16.63" width="95.34" alt="logo">
+        </div>
 
-   <!-- Visibility Toggler here-->
-    <div class="ml-auto"></div>
-    </div>
-    
+        <!-- Visibility Toggler here-->
+        <div class="ml-auto">
+        
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,12 +19,15 @@
 <script>
 export default {
   name: "card",
+
+  data: () => ({}),
 };
 </script>
 
 <style scoped>
 .card-holder {
   background: var(--primary-black);
-
-}
+  padding: 20px 15px 10px 15px;
+  border-radius: 20px;
+  }
 </style>
